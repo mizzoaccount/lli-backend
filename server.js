52244eb3139f6bea364aca22f7ee2b87541990c4
Samuel Hope <samuelhope805@gmail.com>
2025-04-15 14:20:25 +0300

    first commit

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ app.use(
   cors({
     origin: [
       'http://localhost:3000', 
-      'http://localhost:3001', 
+      'http://localhost:3001',
+      'https://www.legislativeleadershipinstitute.com', 
       'https://legislativeleadershipinstitute.onrender.com'
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
